2b338ddd9734f80fd5ba59bd31f484e3dc2bbbaf
Anshuman Bhagat <anshuman.bhagat1999@gmail.com>
2020-10-02 13:11:39 +0530

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 function RenderDish({ dish }) {
   if (dish != null)
@@ -15,34 +24,29 @@ function RenderDish({ dish }) {
   else return <div></div>;
 }
 
-function RenderComments({ dish }) {
-  if (dish != null) {
-    const comments = dish.comments;
-    if (comments != null) {
-      const list = comments.map((comment) => {
-        return (
-          <ul key={comment.id} className="list-unstyled">
-            <li>{comment.comment}</li>
-            <li>
-              -- {comment.author} ,{" "}
-              {new Intl.DateTimeFormat("en-US", {
-                year: "numeric",
-                month: "short",
-                day: "2-digit",
-              }).format(new Date(Date.parse(comment.date)))}
-            </li>
-          </ul>
-        );
-      });
+function RenderComments({ comments }) {
+  if (comments != null) {
+    const list = comments.map((comment) => {
       return (
-        <div className="container">
-          <h4>Comments</h4>
-          {list}
-        </div>
+        <ul key={comment.id} className="list-unstyled">
+          <li>{comment.comment}</li>
+          <li>
+            -- {comment.author} ,{" "}
+            {new Intl.DateTimeFormat("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "2-digit",
+            }).format(new Date(Date.parse(comment.date)))}
+          </li>
+        </ul>
       );
-    } else {
-      return <div></div>;
-    }
+    });
+    return (
+      <div className="container">
+        <h4>Comments</h4>
+        {list}
+      </div>
+    );
   } else {
     return <div></div>;
   }
@@ -52,11 +56,23 @@ const DishDetail = (props) => {
   return (
     <div className="container">
       <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/menu">Menu</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>{props.dish.name}</h3>
+          <hr />
+        </div>
+      </div>
+      <div className="row">
         <div className="col-12 col-md-5 m-1">
           <RenderDish dish={props.dish} />
         </div>
         <div className="col-12 col-md-5 m-1">
-          <RenderComments dish={props.dish} />
+          <RenderComments comments={props.comments} />
         </div>
       </div>
     </div>
